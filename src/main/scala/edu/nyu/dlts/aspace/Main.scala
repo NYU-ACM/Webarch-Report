e49597ab4dcba9a3aaba92285236a43c746bf8f5
Donald Mennerich <don.mennerich@nyu.edu>
2019-04-12 15:56:15 -0400

    updating client, adding page enumeration through query

--- src/main/scala/edu/nyu/dlts/aspace/Main.scala
+++ src/main/scala/edu/nyu/dlts/aspace/Main.scala
@@ -1,11 +1,8 @@
 package edu.nyu.dlts.aspace
 
-import java.io.{File, FileWriter}
+
 
 import edu.nyu.dlts.aspace.AspaceClient.AspaceSupport
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.utils.URIBuilder
-import org.json4s.JsonAST.{JArray, JString, JValue}
 import org.json4s.native.JsonMethods._
 import org.json4s._
 import org.json4s.JsonDSL._
@@ -15,42 +12,25 @@ case class AO(title: String, level: String)
 
 object Main extends App with AspaceSupport {
 
-  val f = new File("test.tsv")
-  val writer = new FileWriter(f)
-  writer.write(s"uri\ttitle\tresource_uri\tscope_note\n")
-  writer.flush()
-
-  val json = "query" ->
+  val query: String = compact(render("query" ->
     ("field" -> "otherlevel") ~
       ("value" -> "website") ~
       ("jsonmodel_type" -> "field_query") ~
       ("negated" -> false) ~
-      ("literal" -> true)
+      ("literal" -> true)))
 
-  val builder = new URIBuilder(env + "/repositories/2/search")
-  builder.setParameter("page", "1")
-  builder.setParameter("page_size", "20")
-  builder.setParameter("aq", compact(render(json)))
-  val httpGet = new HttpGet(builder.build())
-  httpGet.addHeader(header, token.get)
-  val response = get(httpGet)
-  val results = (response.get.json \ "results").extract[List[JValue]]
+  val initialResponse = search(2, 1, 10, query).get.body
 
+  val lastPage = (initialResponse \ "last_page").extract[Int]
+  val totalHits = (initialResponse \ "total_hits").extract[Int]
 
-  results.foreach { result =>
-    val j = parse((result \ "json").extract[String])
-    val uri = (result \ "uri").extract[String]
-    val title = (result \ "title").extract[String]
-    val resource = (result \ "resource").extract[String]
-    val notes = (j \ "notes").extract[JValue]
-    val scope = ((notes(0) \"subnotes")(0) \"content").extract[String]
-    writer.write(s"t$uri\t$title\t$resource\t$scope\n")
-    writer.flush()
-
+  (1 to lastPage).foreach { i =>
+    val response = search(2,i,10,query).get
+    println(s"page $i")
+    val body = response.body
+    val results = (body \ "results").extract[List[JValue]]
+    results.foreach { result => println("\ttitle: " + (result \ "title").extract[String]) }
   }
-
-  writer.flush()
-  writer.close
 
 }
 
